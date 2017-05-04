3ddde3520381edfa888d9d94470af62883e8ee17
RodneyKam <rodney.kam@relayhealth.com>
2017-05-04 14:52:35 -0700

    WOrk in Progress

---
+++ RemoteExecPlayground/MainProgram.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Management;
+using System.Management.Automation;
+using System.Management.Automation.Runspaces;
+using System.Security;
+
+namespace RemoteExecPlayground
+{
+    class Program
+    {
+ 
+        public static void Main()
+        {
+
+            ExecuteTestProcesses();
+            Console.ReadKey();
+        }
+
+        public static void ExecuteTestProcesses()
+        {
+            //Get the object on which the method will be invoked
+            ManagementClass processClass =
+            new ManagementClass("Win32_Process");
+
+            // Option 1: Invocation using parameter objects
+            //================================================
+
+            //Get an input parameters object for this method
+            ManagementBaseObject inParams =
+            processClass.GetMethodParameters("Create");
+
+            //Fill in input parameter values
+            inParams["CommandLine"] = "calc.exe";
+
+            //Execute the method
+            ManagementBaseObject outParams =
+            processClass.InvokeMethod("Create", inParams, null);
+
+            //Display results
+            //Note: The return code of the method is provided
+            // in the "returnValue" property of the outParams object
+            Console.WriteLine("Creation of calculator " +
+                "process returned: " + outParams["returnValue"]);
+            Console.WriteLine("Process ID: " + outParams["processId"]);
+
+            // Option 2: Invocation using args array
+            //=======================================
+
+            //Create an array containing all arguments for the method
+            object[] methodArgs = { "notepad.exe", null, null, 0 };
+
+            //Execute the method
+            object result = processClass.InvokeMethod("Create", methodArgs);
+
+            //Display results
+            Console.WriteLine("Creation of process returned: " + result);
+            Console.WriteLine("Process id: " + methodArgs[3]);
+        }
+
+    }
+
+}
--- RemoteExecPlayground/Program.cs
+++ RemoteExecPlayground/Program.cs
@@ -1,8 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Management.Automation;
+using System.Management.Automation.Runspaces;
+using System.Security;
 
 namespace RemoteExecPlayground
 {
@@ -10,6 +9,47 @@ namespace RemoteExecPlayground
     {
         static void Main(string[] args)
         {
+            var password = new SecureString();
+            Array.ForEach("D3vOp$Admin".ToCharArray(), password.AppendChar);
+            PSCredential credential = new PSCredential("RHCQA\\rhcqaAdmin", password);
+
+            WSManConnectionInfo connectionInfo = new WSManConnectionInfo();
+            connectionInfo.ComputerName = "RHC-HSQADEP01";
+            connectionInfo.Credential = credential;
+            Runspace runspace = RunspaceFactory.CreateRunspace(connectionInfo);
+            runspace.Open();
+            using (PowerShell ps = PowerShell.Create())
+            {
+                ps.Runspace = runspace;
+                ps.AddScript("Get-ChildItem C:\\Windows");
+                var results = ps.Invoke();
+                foreach (var result in results)
+                {
+                    Console.WriteLine(result.Members["FullName"].Value);
+                    Console.WriteLine(result.ToString());
+                }
+            }
+            runspace.Close();
+
+            Console.WriteLine("Done!");
+            Console.ReadKey();
+        }
+
+        public static void LocalPSExec()
+        {
+            Runspace runspace = RunspaceFactory.CreateRunspace();
+            runspace.Open();
+            using (PowerShell ps = PowerShell.Create())
+            {
+                ps.Runspace = runspace;
+                ps.AddScript("Get-Process");
+                var results = ps.Invoke();
+                foreach (var result in results)
+                {
+                    Console.WriteLine(result.ToString());
+                }
+            }
+            runspace.Close();
         }
     }
 }
